95b0ca972518fa9ad6b5277db1b3389da29d6265
Chris Galbraith <chris@chrismade.me>
2015-09-04 20:26:36 +0100

    Remove test page

--- htdocs/theme/default/partials/homepage.php
+++
@@ -1,7 +0,0 @@
-<form action="/form-process" method="post">
-    <p><input type="text" name="name" placeholder="Name"></p>
-    <p><input type="email" name="email" placeholder="Email"></p>
-    <p><input type="tel" name="phone" placeholder="Phone"></p>
-    <p><textarea name="message"></textarea></p>
-    <p><button type="submit">Send</button></p>
-</form>
