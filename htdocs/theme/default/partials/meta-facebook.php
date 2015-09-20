2d4b433149734ba7d4551884bdb65b80b059ac3c
Chris Galbraith <chris@chrismade.me>
2015-09-20 16:14:52 +0100

    Tidy up <head>
    Moved Facebook and Twitter meta tags to their own partials making them
    optional and removed them from <head>.
    
    Added meta tag to force IE to use latest rendering mode.
    
    Removed unnecessary apple touch icon tags.

---
+++ htdocs/theme/default/partials/meta-facebook.php
@@ -0,0 +1,8 @@
+<?php if (isset($this->page['facebook'])): ?>
+<meta property="og:site_name" content="<?php echo $this->page['facebook']['site_name'] ?>">
+<meta property="og:type" content="<?php echo $this->page['facebook']['type'] ?>">
+<meta property="og:title" content="<?php echo $this->page['facebook']['title'] ?>">
+<meta property="og:url" content="<?php echo $this->page['facebook']['url'] ?>">
+<meta property="og:image" content="<?php echo $this->page['facebook']['image'] ?>">
+<meta property="og:description" content="<?php echo $this->page['facebook']['description'] ?>">
+<?php endif; ?>
---
+++ htdocs/theme/default/partials/meta-twitter.php
@@ -0,0 +1,7 @@
+<?php if (isset($this->page['twitter'])): ?>
+<meta name="twitter:card" content="<?php echo $this->page['twitter']['card'] ?>">
+<meta name="twitter:url" content="<?php echo $this->page['twitter']['url'] ?>">
+<meta name="twitter:title" content="<?php echo $this->page['twitter']['title'] ?>">
+<meta name="twitter:description" content="<?php echo $this->page['twitter']['description'] ?>">
+<meta name="twitter:image" content="<?php echo $this->page['twitter']['image'] ?>">
+<?php endif; ?>
